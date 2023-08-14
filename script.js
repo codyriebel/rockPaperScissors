bf8e21ac37a3e4b1e9676222ba3af83a49d3daab
Cody Riebel <cody.riebel@gmail.com>
2023-08-13 20:03:52 -0400

    feat isWinner and playRound needs testing

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-/*function getComputerChoice randomly returns rock, paper, scissors
-init vars as strings with 'rock', 'paper', 'scissors'
+/*
+function getComputerChoice randomly returns rock, paper, scissors
+init choice string
 use Math random with 3 choices
 if 0 return rock etc...
- */
+
 
 function getComputerChoice() {
   let choice = ''
@@ -21,3 +22,51 @@ function getComputerChoice() {
   return choice;
 }
 
+*/
+
+const getComputerChoice = () => {
+  let randomNum = Math.floor(Math.random() * 3);
+  if (randomNum === 0) {
+    return 'rock';
+  } else if (randomNum === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+console.log(getComputerChoice());
+
+/* 
+function playRound(playerSelection, computerSelection)
+return string that declares winner
+case insensitive playerSelection
+
+def win
+if tie, elif win, else lost
+*/
+
+const isWinner = (player, computer) => {
+  if (
+    player === 'rock' && computer === 'scissors' ||
+    player === 'paper' && computer === 'rock' ||
+    player === 'scissors' && computer === 'paper'
+  ) {
+    return true;
+  }
+};
+
+function playRound(playerSelection, computerSelection) {
+  const playerSelectionLower = playerSelection.toLowerCase();
+  if (playerSelection === computerSelection) {
+    return 'Tie!'
+  } else if (isWinner(playerSelection, computerSelection)) {
+    return `Winner! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `Loser! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+ 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
